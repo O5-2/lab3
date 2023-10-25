29a031f021ef888c6edfd0ad9f17215c47ba2c78
O5-2 <35108232+O5-2@users.noreply.github.com>
2023-10-25 16:56:55 -0700

    Fixes array methods.

--- ArrayTests.java
+++ ArrayTests.java
@@ -9,10 +9,22 @@ public class ArrayTests {
     assertArrayEquals(new int[]{ 3 }, input1);
 	}
 
+  @Test
+  public void testRIP1() {
+    int[] input1 = {1, 2};
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{2, 1}, input1);
+  }
 
   @Test
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testR1() {
+    int[] input1 = {0, 1, 1};
+    assertArrayEquals(new int[]{1, 1, 0}, ArrayExamples.reversed(input1));
   }
 }
